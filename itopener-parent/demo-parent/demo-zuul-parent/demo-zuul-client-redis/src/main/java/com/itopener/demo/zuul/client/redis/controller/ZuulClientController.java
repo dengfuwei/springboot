47c236434ec12f6653cfac2a77027fa21c022a60
fuwei.deng <fuwei.deng@integrallocal.msxf.com>
2017-07-07 10:22:39 +0800

    修改zuul route示例代码的client模块名

---
+++ itopener-parent/demo-parent/demo-zuul-parent/demo-zuul-client-redis/src/main/java/com/itopener/demo/zuul/client/redis/controller/ZuulClientController.java
@@ -0,0 +1,23 @@
+package com.itopener.demo.zuul.client.redis.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itopener.demo.zuul.sdk.vo.UserVO;
+import com.itopener.framework.ResultMap;
+import com.itopener.utils.TimestampUtil;
+
+@RestController
+@RequestMapping("zuulclient")
+public class ZuulClientController {
+
+	@RequestMapping("call/{id}")
+	public ResultMap callBalanced(@PathVariable long id){
+		UserVO user = new UserVO();
+		user.setCreateTime(TimestampUtil.current());
+		user.setId(id);
+		user.setName("name" + id);
+		return ResultMap.buildSuccess().put("user", user);
+	}
+}
---
+++ itopener-parent/demo-parent/demo-zuul-parent/demo-zuul-client-zk/src/main/java/com/itopener/demo/zuul/client/zk/filters/ZuulClientFilter.java
@@ -0,0 +1,49 @@
+package com.itopener.demo.zuul.client.zk.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class ZuulClientFilter extends ZuulFilter {
+
+	private final Logger logger = LoggerFactory.getLogger(ZuulClientFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+		Object a = request.getParameter("a");
+		if (a != null) {
+			logger.warn("param a is empty");
+			ctx.setSendZuulResponse(false);
+			ctx.setResponseStatusCode(401);
+			return false;
+		}
+		logger.info("param a is : " + a);
+		return true;
+	}
+
+	@Override
+	public String filterType() {
+		return FilterConstants.PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
